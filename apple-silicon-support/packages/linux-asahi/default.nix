ac2686f7e93668664ab8a8da2e4691dab807e098
Guillaume Nanty <7790572+KongrooParadox@users.noreply.github.com>
2026-08-01 02:39:21 +0200

    fairydust branch for dp-alt mode over usb-c

--- apple-silicon-support/packages/linux-asahi/default.nix
+++ apple-silicon-support/packages/linux-asahi/default.nix
@@ -29,12 +29,11 @@ let
         version = "7.1.13";
         modDirVersion = version;
         extraMeta.branch = "7.1";
-
         src = fetchFromGitHub {
           owner = "AsahiLinux";
           repo = "linux";
-          tag = "asahi-7.1.13-3";
-          hash = "sha256-quvdcQ2LbYQyCDQFKc6KPjWv+f5fpWfUrXo5Id0kpwE=";
+          rev = "ce9f2eba72c061a50b2d790450e90af3439d8c24"; # fairydust branch at version 7.1.13
+          hash = "sha256-W3yMSUe6xa+M/X0k86kbCS4g3d7jJmO3WV9L/5rQRhI=";
         };
 
         kernelPatches = [
